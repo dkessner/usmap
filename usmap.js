c847fe8a84481ca6786fe51c9fd8e4359b28e084
Darren Kessner <dkessner@eitm.org>
2024-08-16 08:32:22 -0700

    modified:   usmap.js
    added point transformation

--- usmap.js
+++ usmap.js
@@ -1,19 +1,41 @@
+//
+// usmap.js
+//
 
 
-
-console.log(state_data['California'])
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
 }
 
-let x = 50;
+
+function transform_coordinates(p) {
+    const x1 = -120;
+    const x2 = -70;
+    const y1 = 30;
+    const y2 = 50;
+    return [map(p[0], x1, x2, 0, width),
+            map(p[1], y1, y2, height, 0)]
+}
+
 
 function draw() {
-  background(0);
-  ellipse(x, 200, 100, 50);
-  x++;
 
-  if (x > width+50) 
-    x = -50;
+    background(0);
+    noFill();
+    stroke(0, 255, 0);
+
+    points = state_data['California'][5]
+    translate(120, 30);
+
+    beginShape();
+
+    for (let p of points)
+    {
+        //console.log(p[0] + ", " + p[1]);
+        q = transform_coordinates(p); 
+        vertex(q[0], q[1]);
+    }
+
+    endShape();
 }
